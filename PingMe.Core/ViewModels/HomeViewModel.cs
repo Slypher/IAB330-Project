019f37cbbd48111b31e78566afb4e97618359ba5
LEECH-SURFACE\liamd <liamdidato@gmail.com>
2016-11-01 14:37:36 +1000

    Searching/Filtering

--- PingMe.Core/ViewModels/HomeViewModel.cs
+++ PingMe.Core/ViewModels/HomeViewModel.cs
@@ -1,20 +1,58 @@
 
 using MvvmCross.Core.ViewModels;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 
 namespace PingMe.Core.ViewModels {
     public class HomeViewModel : MvxViewModel {
 
-        // Binds to the value of the searchbar on MembersTab
-        private string search = "";
+        public HomeViewModel() {
+
+            // Create example groups:
+            groups.Add(new Group("Group 1"));
+            groups.Add(new Group("Group 2"));
+            groups.Add(new Group("Another Group"));
+
+            groups[0].AddGenericPeople();
+            groups[1].Members.Add(new GroupMember("Harold", 7));
+            groups[1].Members.Add(new GroupMember("Jeremiah", 555));
+            groups[2].AddGenericPeople();
+            groups[2].Members.Add(new GroupMember("Bobby", 12));
+
+            // Make Group 1 the selected group to start with
+            currentGroupMembers = Groups[0].Members;
+
+            filteredGroupMembers = currentGroupMembers;
+
+            Debug.WriteLine("Test");
+            Debug.WriteLine("Length of filtered list" + currentGroupMembers.Count().ToString());
+        }
+        
+        public void FilterMembers() {
+            if (string.IsNullOrEmpty(Search)) {
+                // Set the filtered list to equal its unfiltered form
+                FilteredGroupMembers = currentGroupMembers;
+            } else {
+                // Filter the list
+                FilteredGroupMembers = new ObservableCollection<GroupMember>(
+                    currentGroupMembers.Where(
+                    o => o.Name.ToLower().Contains(Search.ToLower())).ToList());
+                RaisePropertyChanged(() => FilteredGroupMembers);
+            }
+        }
+
+        // Binds to the value of the searchbar
+        private string search;
         public string Search {
             get { return search; }
             set {
-                if (value != null && value != search) {
-                    search = value;
-                    RaisePropertyChanged(() => Search);
-                   
-                }
+                search = value;
+                
+                RaisePropertyChanged(() => Search);
+
+                FilterMembers();
+                
             }
         }
 
@@ -38,40 +76,43 @@ namespace PingMe.Core.ViewModels {
             }
             set {
                 selectedGroup = value;
-                RaisePropertyChanged(() => SelectedGroup);
                 // When selected group changes, change the list:
                 CurrentGroupMembers = value.Members;
+
+                RaisePropertyChanged(() => SelectedGroup);
+
+                FilterMembers();
             }
         }
 
+
+        /*
+         * The list of members to display in the listview
+         * The filtered form is after the search has been done
+         */
 
         private ObservableCollection<GroupMember> currentGroupMembers = new ObservableCollection<GroupMember>();
         public ObservableCollection<GroupMember> CurrentGroupMembers {
-            get {
-                return currentGroupMembers;
-            }
+            get { return currentGroupMembers; }
             set {
-                SetProperty(ref currentGroupMembers, value);
+                currentGroupMembers = value;
+                RaisePropertyChanged(() => CurrentGroupMembers);
             }
         }
 
 
-        public HomeViewModel() {
-
-            // Create example groups:
-            groups.Add(new Group("Group 1"));
-            groups.Add(new Group("Group 2"));
-            groups.Add(new Group("Another Group"));
-
-            groups[0].AddGenericPeople();
-            groups[1].Members.Add(new GroupMember("Harold", 7));
-            groups[1].Members.Add(new GroupMember("Jeremiah", 555));
-            groups[2].AddGenericPeople();
-            groups[2].Members.Add(new GroupMember("Bobby", 12));
-
-            // Make Group 1 the selected group to start with
-            CurrentGroupMembers = Groups[0].Members; 
+        private ObservableCollection<GroupMember> filteredGroupMembers = new ObservableCollection<GroupMember>();
+        public ObservableCollection<GroupMember> FilteredGroupMembers {
+            get { return filteredGroupMembers; }
+            set {
+                filteredGroupMembers = value;
+                RaisePropertyChanged(() => FilteredGroupMembers);
+            }
         }
+
+
+
+        
 
     }
 }
