6ac2f5d24c3141b73916aeed2db92c253f3baf4b
LEECH-SURFACE\liamd <liamdidato@gmail.com>
2016-11-02 20:17:28 +1000

    Linked SendLocation and Notifications, some fixes

--- PingMe.Core/ViewModels/NotificationsViewModel.cs
+++ PingMe.Core/ViewModels/NotificationsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Platform;
 using PingMe.Core.Classes;
 using System;
 using System.Collections.Generic;
@@ -6,13 +8,32 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace PingMe.Core.ViewModels {
     public class NotificationsViewModel : MvxViewModel {
 
         public NotificationsViewModel() {
-            // Pull notification data
+            // Pull notification data 
             Notifications = DataModel.Notifications;
+        }
+
+
+        /*
+         * When selecting item from list
+         */
+        private MvxCommand<Notification> itemSelectedCommand;
+
+        public System.Windows.Input.ICommand ItemSelectedCommand {
+            get {
+                itemSelectedCommand = itemSelectedCommand ?? new MvxCommand<Notification>(DoSelectItem);
+                return itemSelectedCommand;
+            }
+        }
+
+        private void DoSelectItem(Notification item) {
+            int id = DataModel.Notifications.IndexOf(item);
+            ShowViewModel<SendLocationViewModel>(new { id = id });
         }
 
 
--- PingMe.Core/ViewModels/SendLocationViewModel.cs
+++ PingMe.Core/ViewModels/SendLocationViewModel.cs
@@ -1,4 +1,7 @@
 using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Platform;
+using PingMe.Core.Classes;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
@@ -11,6 +14,29 @@ namespace PingMe.Core.ViewModels {
 
         }
 
+        /*
+         * Contains the user that was requesting location as well as the time that user requested
+         */ 
+        private Notification requestData;
+        public Notification RequestData {
+            get { return requestData; }
+            set { requestData = value; }
+        }
+
+        /*
+         * Text to display at the top (with user name)
+         */
+        private string titleText;
+        public string TitleText {
+            get { return titleText; }
+            set { titleText = value; RaisePropertyChanged(() => TitleText); }
+        }
+
+        public void Init(int id) {
+            RequestData = DataModel.Notifications[id];
+            // Set the title text
+            TitleText = "Send " + RequestData.Sender.Name + " your location.";
+        }
 
 
     }
